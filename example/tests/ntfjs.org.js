5323addedf2cfd1e42218d471b2ef96b70a64276
Silas Sewell <silas@sewell.org>
2012-03-27 23:01:30 -0400

    Add failing test to example

--- example/tests/ntfjs.org.js
+++ example/tests/ntfjs.org.js
@@ -1,8 +1,13 @@
 var ntf = require('ntf')
   , test = ntf.http('http://ntfjs.org')
 
-exports.ntf = test.get('/', function(test) {
+exports.frontpage = test.get('/', function(test) {
   test.statusCode(200)
   test.body('ntf')
+  test.done()
+})
+
+exports.fail = test.get('/fail', function(test) {
+  test.statusCode(200)
   test.done()
 })
